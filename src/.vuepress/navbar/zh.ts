faab6899aeac584b141f791ecd93505d0186e968
王天赐 <15565946702@163.com>
2023-01-14 00:05:45 +0800

    王天赐, feat: 二叉树的深度

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -5,28 +5,29 @@ export const zhNavbar = navbar([
     "/jvm/",
     "/service/",
     "/blog/",
-    {
-        text: "LeetCode刷题系列",
-        icon: "note",
-        prefix: "/leetcode/",
-        children: [
-            {
-                text: "二叉树系列",
-                icon: "edit",
-                prefix: "binary_tree/",
-                children: [
-                    {
-                        text: "二叉树的遍历",
-                        icon: "edit",
-                        link: "二叉树的遍历",
-                    },
-                    {
-                        text: "其他",
-                        icon: "edit",
-                        link: "其他",
-                    }
-                ]
-            }
-        ]
-    },
+    "/leetcode/",
+    // {
+    //     text: "LeetCode刷题系列",
+    //     icon: "note",
+    //     prefix: "/leetcode/",
+    //     children: [
+    //         {
+    //             text: "二叉树系列",
+    //             icon: "edit",
+    //             prefix: "binary_tree/",
+    //             children: [
+    //                 {
+    //                     text: "二叉树的遍历",
+    //                     icon: "edit",
+    //                     link: "二叉树的遍历",
+    //                 },
+    //                 {
+    //                     text: "其他",
+    //                     icon: "edit",
+    //                     link: "其他",
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // },
 ]);
--- src/leetcode/README.md
+++ src/leetcode/README.md
@@ -1,3 +1,4 @@
 ---
 title: leetcode
+icon: note
 ---
